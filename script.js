39f83579e5a01638b059c24fee609950b00431c8
Karol <karol.gratus@gmail.com>
2017-11-15 15:22:20 +0100

    fighting with click listeners

--- script.js
+++ script.js
@@ -11,23 +11,25 @@ class Column {
   constructor (name) {
     this.id = randomString();
     this.name = name;
-    this.element = createColumn();
+    this.element = this.createColumn();
   }
   
   createColumn() {
     let column = document.createElement("div");
     column.setAttribute("class", "column");
-    column.appendChild(createColumnTitle());
-    column.appendChild(createColumnDeleteButton());
-    column.appendChild(createColumnAddCardButton());
-    column.appendChild(creaeColumnCardList());
+    
+    column.appendChild(this.createColumnTitle());
+    column.appendChild(this.createColumnDeleteButton());
+    column.appendChild(this.createColumnAddCardButton());
+    column.appendChild(this.createColumnCardList());
+        
     return column;
   }
   
   createColumnTitle() {
     let columnTitle = document.createElement("h2");
     columnTitle.setAttribute("class", "column-title");
-    columnTitle.innerHTML(self.name);
+    columnTitle.innerHTML = this.name;
     return columnTitle;
   }
 
@@ -40,34 +42,98 @@ class Column {
   createColumnDeleteButton() {
     let columnDelete = document.createElement("button");
     columnDelete.setAttribute("class", "btn-delete");
-    columnDelete.innerHTML("x");
+    columnDelete.innerHTML = "x";
+
+    columnDelete.onclick = () => {
+      self.removeColumn();
+    }
+
     return columnDelete;
   }
     
   createColumnAddCardButton() {
     let columnAddCard = document.createElement("button");
-    columnDelete.setAttribute("class", "add-card");
-    columnAddCard.innerHTML("Add a card");
-    return columnAddCard;
-  }
+    columnAddCard.setAttribute("class", "add-card");
+    columnAddCard.innerHTML = "Add a card";
 
     columnAddCard.onclick = () => {
       self.addCard(new Card(prompt("Enter the name of the card")));
     }
-    columnDelete.onclick = () => {
-      self.removeColumn();
-    }
-   
-  
+
+    return columnAddCard;
+  }
 }
 
-Column.prototype = {
-  addCard: function(card) {
+Column.prototype.addCard = function(card) {
     this.$element.children('ul').append(card.$element);
-  },
-  removeColumn: function() {
+}
+
+Column.prototype.removeColumn = function() {
     this.$element.remove();
+}
+
+class Card {
+  constructor (name) {
+    this.id = randomString();
+    this.name = name;
+    this.element = createCard();
   }
+
+  createCard() {
+    let card = document.createElement("li");
+    card.setAttribute("class", "card");
+
+    card.appendChild(createCardDescription());
+    card.appendChild(createCardDeleteButton());
+    
+    cardDelete.onclick = () => {
+      self.removeCard();
+    }
+  }
+  
+  createCardDescription() {
+    let cardDescription = document.createElement("p");
+    cardDescription.setAttribute("class", "card-description");
+    cardDescription.innerHTML(self.description);
+    return cardDescription;
+  }
+  
+  createCardDeleteButton() {
+    let cardDelete = document.createElement("button");
+    cardDelete.setAttribute("class", "btn-delete");
+    cardDelete.innerHTML("x");
+    return cardDelete;
+  }
+}
+
+Card.prototype.removeCard = function() {
+		this.$element.remove();
+}
+
+
+let board = {
+  name: 'Kanban Board',
+  addColumn: function(column) {
+    (console.log(column))
+    this.$element.append(column.element.innerHTML);
+    initSortable();
+  },
+  $element: $('#board .column-container')
 };
+
+function initSortable() {
+  $('.column-card-list').sortable({
+    connectWith: '.column-card-list',
+    placeholder: 'card-placeholder'
+  }).disableSelection();
+}
+
+$('.create-column')
+.click(function(){
+const name = prompt('Enter a column name');
+let column = new Column(name);
+
+board.addColumn(column);
+});
 
 
