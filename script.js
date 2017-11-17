e78a02421a6c5b0d77ad12632f927328312ec2f5
Karol <karol.gratus@gmail.com>
2017-11-17 14:04:34 +0100

    finihed

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ class Board {
     this.name = 'Kanban Board';
   }
   
-  addElement(child, parent=document.querySelector('#board .column-container')) {
+  static addElement(child, parent=document.querySelector('#board .column-container')) {
     parent.append(child)
     initSortable();
   }
   
-  removeElement(e) {e.remove()}
+  static removeElement(e) {e.remove()}
 
-  createDeleteButton() {
+  static createDeleteButton() {
     let deleteButton = document.createElement("button");
     deleteButton.className = "btn-delete";
     deleteButton.textContent = "x";
@@ -29,7 +29,7 @@ class Column extends Board {
   createColumn() {
     let column = document.createElement("div");
     column.className = "column";
-    column.appendChild(this.createDeleteButton());
+    column.appendChild(Board.createDeleteButton());
     column.appendChild(this.createColumnTitle());
     column.appendChild(this.createColumnAddCardButton());
     column.appendChild(this.createColumnCardList());
@@ -52,7 +52,7 @@ class Column extends Board {
   createColumnAddCardButton() {
     let columnAddCard = document.createElement("button");
     columnAddCard.className = "add-card";
-    columnAddCard.textContent = "Add a card";
+    columnAddCard.textContent = "+";
     return columnAddCard;
   }
 }
@@ -69,7 +69,7 @@ class Card extends Board {
     let card = document.createElement("li");
     card.className = "card";
     card.appendChild(this.createCardDescription());
-    card.appendChild(this.createDeleteButton());
+    card.appendChild(Board.createDeleteButton());
     return card;
   }
   
@@ -102,24 +102,24 @@ function randomString() {
   mainBoard.addEventListener('click', (e) => {
     if (e.target.matches('.btn-delete')) {
         const elementClicked = e.target;
-        Card.prototype.removeElement(elementClicked.parentNode);
+        Board.removeElement(elementClicked.parentNode);
     } else if (e.target.matches('.add-card')) {
         const elementClicked = e.target;
         const card = new Card(prompt("Enter the name of the card"));
-        card.addElement(card.element, elementClicked.parentNode.children[3]);
+        Board.addElement(card.element, elementClicked.parentNode.children[3]);
     } else if (e.target.matches('.create-column')) {
         const column = new Column(prompt('Enter a column name'));
-        column.addElement(column.element);
+        Board.addElement(column.element);
     }
   });
 })()
 
 function setup(columnName, cardName) {
   const newColumn = new Column(columnName);
-  newColumn.addElement(newColumn.element)
+  Board.addElement(newColumn.element)
   if (cardName) {
     const card = new Card(cardName);
-    newColumn.addElement(card.element, newColumn.element.childNodes[3]);
+    Board.addElement(card.element, newColumn.element.childNodes[3]);
   }
 }
 
