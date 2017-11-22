7efd7f21f27d42f606f5608175df8e54c1212d7c
Karol <karol.gratus@gmail.com>
2017-11-22 14:57:22 +0100

    decidet to make new branch for api implementation

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
-};
 
 class Board {
   constructor (name) {
